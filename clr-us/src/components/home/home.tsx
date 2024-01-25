3e5068e5b99fdd217145791a71442bc73ce25a8b
judy-n <judynaamani@icloud.com>
2024-01-25 02:09:37 -0500

    install mui & react-router-dom

---
+++ clr-us/src/components/home/home.tsx
@@ -0,0 +1,12 @@
+import { Typography, Box } from "@mui/material";
+import React from "react";
+
+export const Home: React.FC = () => {
+    return (
+        <Box>
+            <Typography variant="h1" component="h1">
+                Home
+            </Typography>
+        </Box>
+    )
+}
