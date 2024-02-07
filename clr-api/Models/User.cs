1687a34be6bb0688342be35c89629a85ef8c1a20
Julien <jbertazzolambert@gmail.com>
2024-02-07 00:52:47 -0500

    feature: user courses and roles in models

--- clr-api/Models/User.cs
+++ clr-api/Models/User.cs
@@ -3,6 +3,25 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace clr_api.Models;
 
+public enum UserRole
+{
+    Student,
+    Instructor
+}
+
+public class UserCourse
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Oid { get; set; }
+    
+    [BsonElement("code")]
+    public string? Code { get; set; }
+    
+    [BsonElement("role")]
+    public UserRole Role { get; set; }
+}
+
 public class User
 {
     [BsonId]
@@ -14,4 +33,7 @@ public class User
 
     [BsonElement("password")]
     public string Password { get; set; } = null!;
+
+    [BsonElement("courses")]
+    public List<UserCourse> Courses { get; set; } = new List<UserCourse>();
 }
