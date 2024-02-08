e741c21eb94458f6384d6dac75946c34c62ea1dc
Julien <jbertazzolambert@gmail.com>
2024-02-08 15:54:38 -0500

    feature: CRUD ops on CLRS problem bank

--- clr-api/Models/ClrApiDatabaseSettings.cs
+++ clr-api/Models/ClrApiDatabaseSettings.cs
@@ -9,4 +9,6 @@ public class ClrApiDatabaseSettings
     public string UsersCollectionName { get; set; } = null!;
     
     public string ClassCollectionName { get; set; } = null!;
+
+    public string CLRSCollectionName { get; set; } = null!;
 }
