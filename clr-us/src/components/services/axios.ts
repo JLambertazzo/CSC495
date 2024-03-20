0fe3e81f7e75ca3f3380a198fdc7c12723b24d2e
Julien <jbertazzolambert@gmail.com>
2024-03-20 16:21:13 -0400

    fix: missed api part of route

--- clr-us/src/components/services/axios.ts
+++ clr-us/src/components/services/axios.ts
@@ -3,7 +3,7 @@ import Axios from 'axios'
 export const axios = Axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? 'https://clrus.azurewebsites.net/'
+      ? 'https://clrus.azurewebsites.net/api'
       : 'http://localhost:5277/api/',
   headers: {
     'Content-type': 'application/json',
